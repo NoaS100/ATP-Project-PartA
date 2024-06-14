3637c65581172a550cc28cd437ead83ed9c76f99
noash <shvets@post.bgu.ac.il>
2024-06-14 18:25:09 +0300

    Added searching algorithms.

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -3,7 +3,7 @@ package algorithms.mazeGenerators;
 import java.util.Random;
 import java.util.Arrays;
 
-public class Maze {
+public class Maze{
 
     private Position startPosition;
     private Position endPosition;
@@ -18,19 +18,19 @@ public class Maze {
         this.columns = columns;
     }
 
-    public int getRows() {
+    public int getRows(){
         return rows;
     }
 
-    public int getColumns() {
+    public int getColumns(){
         return columns;
     }
 
-    public Position getStartPosition() {
+    public Position getStartPosition(){
         return startPosition;
     }
 
-    public Position getGoalPosition() {
+    public Position getGoalPosition(){
         return endPosition;
     }
 
@@ -45,6 +45,7 @@ public class Maze {
     }
 
 
+
     public void makeGoalPosition() {
         if (endPosition == null && rows > 0 && columns > 0) {
             boolean flag = true;
@@ -54,9 +55,9 @@ public class Maze {
             while (flag) {
                 if (mazeArray[endPos.getRowIndex()][endPos.getColumnIndex()] == 0 &&
                         endPos.getColumnIndex() != startPosition.getColumnIndex() &&
-                        endPos.getRowIndex() != startPosition.getRowIndex()) {
-                    flag = false;
-                } else { // bad choice - keep random
+                        endPos.getRowIndex() != startPosition.getRowIndex()){
+                    flag = false;}
+                else { // bad choice - keep random
                     endPos = chooseRandomly();
                 }
             }
@@ -81,9 +82,10 @@ public class Maze {
     }
 
 
+
     public Position chooseRandomly() {
         Random random = new Random();
-        int cases = random.nextInt(4);
+        int cases= random.nextInt(4);
         Position randomlyPos = new Position(random.nextInt(rows), random.nextInt(columns));
 
         if (cases == 0) {//first row
@@ -91,36 +93,24 @@ public class Maze {
             randomlyPos.setColumnIndex(random.nextInt(columns));
         }
         if (cases == 1) {//last row
-            randomlyPos.setRowIndex(rows - 1);
+            randomlyPos.setRowIndex(rows-1);
             randomlyPos.setColumnIndex(random.nextInt(columns));
         }
-        if (cases == 2) {//first column
+        if (cases == 2){//first column
             randomlyPos.setRowIndex(random.nextInt(rows));
             randomlyPos.setColumnIndex(0);
         }
-        if (cases == 3) { //last column
+        if (cases == 3){ //last column
             randomlyPos.setRowIndex(random.nextInt(rows));
-            randomlyPos.setColumnIndex(columns - 1);
+            randomlyPos.setColumnIndex(columns-1);
         }
 
         return randomlyPos;
     }
 
 
-    public boolean checkPosition(Position position) {
-        int rowIndex = position.getRowIndex();
-        int columnIndex = position.getColumnIndex();
-        return checkPosition(rowIndex, columnIndex);
-    }
-
-    public boolean checkPosition(int rowIndex, int columnIndex) {
-        return rowIndex < getRows() && rowIndex > -1 &&
-                columnIndex < getColumns() && columnIndex > -1 &&
-                mazeArray[rowIndex][columnIndex] == 0;
-    }
-
-    public void print() {
-        if (startPosition != null && endPosition != null) {
+    public void print(){
+        if(startPosition != null && endPosition != null) {
             char[][] charMazeArr = new char[rows][columns];
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
@@ -128,11 +118,10 @@ public class Maze {
                 }
             }
 
-            charMazeArr[endPosition.getRowIndex()][endPosition.getColumnIndex()] = 'E';
-            charMazeArr[startPosition.getRowIndex()][startPosition.getColumnIndex()] = 'S';
+            charMazeArr[endPosition.getRowIndex()][endPosition.getColumnIndex()]='E';
+            charMazeArr[startPosition.getRowIndex()][startPosition.getColumnIndex()]='S';
             for (int i = 0; i < rows; i++) {
-                System.out.println(Arrays.toString(charMazeArr[i]));
-            }
+                System.out.println(Arrays.toString(charMazeArr[i]));}
         }
     }
 }
